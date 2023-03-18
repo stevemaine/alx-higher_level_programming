d6dbfbb57c02cd239035afb3cd6aed3b37efd5a3
Stephen Isoboye <stephenjasper38@gmail.com>
2023-03-18 09:01:58 -0700

    All Tasks

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    number = 0
+    for element in set(my_list):
+        number += element
+    return numbeo
+t@5d37b35107eb:/alx-higher_level_programmingo
+t@5d37b35107eb:/alx-higher_level_programming/
