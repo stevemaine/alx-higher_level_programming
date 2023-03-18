d1f757ac3107d5b76b39b758c609ffb85d439dbf
Stephen Isoboye <stephenjasper38@gmail.com>
2023-03-18 10:20:06 -0700

    All Tasks

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,4 @@ def uniq_add(my_list=[]):
     number = 0
     for element in set(my_list):
         number += element
-    return numbeo
-t@5d37b35107eb:/alx-higher_level_programmingo
-t@5d37b35107eb:/alx-higher_level_programming/
+    return number
